ae90a901f95d2b761813af78fc9f8f6564bfc74c
freemankevin <freemankevin2017@gmail.com>
2026-02-05 18:38:54 +0800

    2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
   images: {
     unoptimized: true
   },
   trailingSlash: true,
   basePath: '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://judu.freemankevin.uk' : '',
 }
 
 module.exports = nextConfig
